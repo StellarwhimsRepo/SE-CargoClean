685f1a8246e4b469a068ea19818a2894c246aa44
Tostito <tostitogaming@live.com>
2014-11-05 21:56:59 -0600

    initial commits

---
+++ NPCandCargoClean.ps1
@@ -0,0 +1,30 @@
+﻿    $filePath = 'yoursavepath\SANDBOX_0_0_0_.sbs'
+    $filePath2 = 'yoursavepath\SANDBOX.sbc'
+
+    [xml]$myXML = Get-Content $filePath
+    $ns = New-Object System.Xml.XmlNamespaceManager($myXML.NameTable)
+    $ns.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+    [xml]$myXML2 = Get-Content $filePath2
+    $ns2 = New-Object System.Xml.XmlNamespaceManager($myXML2.NameTable)
+    $ns2.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+    #cargo ship and related save cleanup.
+    Write-Host -ForegroundColor Green "Cargo Ship Cleaning ....  "
+    [string]$compare = "360"
+    $NPCPlayerID = $myXML2.SelectNodes("//Identities/MyObjectBuilder_Identity/PlayerId" , $ns2)
+    ForEach($ID in $NPCPlayerID){
+        $NPCID = [string]$ID.InnerText[0] + [string]$ID.InnerText[1] + [string]$ID.InnerText[2]
+        If($NPCID -eq $compare){
+            Write-Host -ForegroundColor Green " NPC ID and owned blocks deleteing ... "
+            $NPCnodeOwns = $myXML.SelectNodes("//CubeBlocks/MyObjectBuilder_CubeBlock[Owner='$($ID.InnerText)']"  , $ns)
+            ForEach($NPCOwned in $NPCnodeOwns){
+            Write-Host -ForegroundColor Green " $($NPCOwned.SubtypeName) deleted!"
+            $NPCOwned.ParentNode.RemoveChild($NPCOwned)
+            }
+            $ID.ParentNode.ParentNode.RemoveChild($ID.ParentNode)
+        }
+    }
+
+    $myXML.Save($filePath)
+    $myXML2.Save($filePath2)
---
+++ README.md
@@ -0,0 +1,22 @@
+SE-CargoClean
+================
+
+NPC and Cargo Ship Clean
+
+**INTENDED FOR DEDICATED SERVERS
+
+If you intend to use these for commercial panel / virtual servers / personal use, go ahead. I just ask that you give me credit for it or leave a testimony of how it has benefited you here: http://forums.keenswh.com/post?id=7099011
+
+Thank you!
+
+The author of this script is not responsible for any damage to the game save or the machine it is run on.
+
+NPC and Cargo Ship Clean
+
+This powershell script is used by changing the variables at the top of the 'ps1' file to match your server settings. 
+
+The default settings will delete NPC owned blocks and their fake player identity in your save when run. This is best paired with a No beacon script or automation to remove ships with no beacons.
+
+Be sure to shut your space engineers server down before running this script!!!
+
+Use the windows shortcut provided to run. Be sure the shortcut and the script file are in the same folder.
